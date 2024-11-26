f6f8cf9796ab1824c7e840aa645de6a1e0b021a2
jeovana Barbosa <jeovana.nascimento@aluno.senai.br>
2024-11-26 10:32:29 -0500

    commit de teste do github

---
+++ Hoje.py
@@ -0,0 +1,7 @@
+chuva = []
+dias = ["seg","ter","quar","qui","sex","sab","dom"]
+cont = 0
+while cont < 7:
+    quantidade = float(input())
+    print("digite a quantidade de chuva para:",(dias[cont]))
+                    
